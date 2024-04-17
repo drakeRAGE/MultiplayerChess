321d03b39fab2f4d04c05263524fa3d030068fde
Dragbos <145892326+drakeRAGE@users.noreply.github.com>
2024-04-17 22:16:11 +0530

    setting move functinality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 import ChessBoard from 'chessboardjsx'
+import {Chess} from 'chess.js'
+
+
+
+const style = {
+  marginTop: '2rem',
+  display: 'flex',
+  justifyContent : 'space-around',
+  alignItem: 'center',
+}
 
 function App() {
+  const [fen, setFen] = useState('start')
+
+  let game = useRef(null);
+
+  // const chess = new Chess()
+
+  useEffect(()=>{
+    game.current = new Chess()
+  }, [])
+  // chess.move("e4")
+  // console.log(chess)
+  console.log(game)
+
+  const onDrop = ({sourceSquare, targetSquare}) => {
+    let move = game.current.move({
+      from : sourceSquare,
+      to : targetSquare,
+    })
+
+    console.log(move)
+}
+  
   return (
-    <div className='App'>
-      <ChessBoard position='start' />
+    <div style={style}>
+      <ChessBoard position={fen} 
+      onDrop={ onDrop }
+      />
+      
+
     </div>
   )
 }
